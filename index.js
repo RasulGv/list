0fd839d26cc3f686a1638978bb3c5c6eed4cf1bf
RasulGv <garavaliyevrasul531@gmail.com>
2024-04-01 00:06:37 +0400

    list

---
+++ index.js
@@ -0,0 +1,42 @@
+let input = document.querySelector('#toDoInput');
+let button = document.querySelector('#addButton');
+let list = document.querySelector('#toDoList');
+let arr = [];
+
+button.addEventListener('click', function() {
+  const task = input.value.trim();
+  if (!task) {
+    alert('Please enter a task');
+    return;
+  }
+
+
+  const li = document.createElement('li');
+  li.textContent = task;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.addEventListener('click', function() { 
+    const index = arr.indexOf(li.textContent);
+    arr.splice(index, 1);
+    list.removeChild(li);
+  });
+
+  const editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  editButton.addEventListener('click', function() { 
+    const newTask = prompt('Edit:', li.textContent);
+    if (newTask) {
+      li.textContent = newTask;
+      arr[arr.indexOf(li.textContent)] = newTask;
+    }
+  });
+
+  
+  li.appendChild(deleteButton);
+  li.appendChild(editButton);
+  list.appendChild(li);
+
+  arr.push(task);
+  input.value = '';
+});
